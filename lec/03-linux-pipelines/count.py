ba4cb0566e05db57d2da18dffd5de4dd5cfd4308
MBadriNarayanan <MBadriNarayanan>
2025-02-09 21:09:53 -0600

    [ ADD ] - Lecture Files

--- lec/03-linux-pipelines/count.py
+++ lec/03-linux-pipelines/count.py
@@ -10,6 +10,6 @@ with open(Path("data") / Path("spotify.csv")) as FH:
     csv_file = csv.reader(FH)
     for line in csv_file:
         count += 1
-        time.sleep(1)  # sleep 1s
+        time.sleep(1) # sleep 1s
 
 print(count)
